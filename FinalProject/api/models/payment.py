2105c83b712cd01d1dd0bc31a890c419879b2f0b
Lauren <houseoflew@live.com>
2026-07-12 18:10:54 -0400

    feat: added payment and promo_codes schemas

--- FinalProject/api/models/payment.py
+++ FinalProject/api/models/payment.py
@@ -2,3 +2,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
+
+class Payment(Base):
+    __tablename__ = "payment"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
