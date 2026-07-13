f188923b29f3461419e52066f3a77afcba110f54
Lauren <houseoflew@live.com>
2026-07-12 21:01:43 -0400

    feat: added features to payment model

--- FinalProject/api/schemas/payment.py
+++ FinalProject/api/schemas/payment.py
@@ -5,5 +5,17 @@ class PaymentBase(BaseModel):
     payment_type: str
     payment_status: str
     card_type: str
-    card_status: str
+    card_number: int
     card_expiry_date: datetime
+    confirmation_code: int
+
+class PaymentCreate(PaymentBase):
+    pass
+
+
+class PaymentResponse(PaymentBase):
+    id: int
+    generated_at: datetime
+
+    class Config:
+        from_attributes = True
