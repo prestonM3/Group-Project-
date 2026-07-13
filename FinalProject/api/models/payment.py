9753c319a4998857e8d14121e4957cb0ad4da456
Lauren <houseoflew@live.com>
2026-07-12 20:59:12 -0400

    feat: added additional feature to payment model

--- FinalProject/api/models/payment.py
+++ FinalProject/api/models/payment.py
@@ -11,6 +11,7 @@ class Payment(Base):
     payment_status = Column(String(8), index=True, nullable=False)
     card_type = Column(String(8), index=True, nullable=False)
     card_number = Column(Integer, index=True, nullable=False)
+    card_expiry_date = Column(DATETIME, nullable=False)
     confirmation_code = Column(Integer, index=True, nullable=False)
 
     # Functions
--- FinalProject/api/schemas/payment.py
+++ FinalProject/api/schemas/payment.py
@@ -2,3 +2,8 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class PaymentBase(BaseModel):
+    payment_type: str
+    payment_status: str
+    card_type: str
+    card_status: str
+    card_expiry_date: datetime
