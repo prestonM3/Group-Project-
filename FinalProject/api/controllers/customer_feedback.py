1e46717f760a2fc7da47c19adb9b9e51753d038e
tachaney <tchaney3@charlotte.edu>
2026-07-30 12:10:15 -0400

    feat: added api endpoints for customer feedback

---
+++ FinalProject/api/controllers/customer_feedback.py
@@ -0,0 +1,65 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import customer_feedback as model
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def create(db: Session, request):
+    new_review = model.CustomerFeedback(
+        order_id=request.order_id,
+        star_rating=request.star_rating,
+        written_review=request.written_review
+    )
+
+    try:
+        db.add(new_review)
+        db.commit()
+        db.refresh(new_review)
+    except SQLAlchemyError as e:
+        error = str(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_review
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.CustomerFeedback).all()
+    except SQLAlchemyError as e:
+        error = str(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.CustomerFeedback).filter(model.CustomerFeedback.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+def update(db: Session, item_id, request):
+    try:
+        item = db.query(model.CustomerFeedback).filter(model.CustomerFeedback.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.CustomerFeedback).filter(model.CustomerFeedback.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
